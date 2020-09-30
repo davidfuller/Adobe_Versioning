2b442f1e6a26a7583f9289349cec5f0fb5d5a9e3
mrhillesq <mrhillesq@gmail.com>
2020-09-30 11:58:37 +0100

    Added Matt's folder for xml folder

--- globals.jsx
+++ globals.jsx
@@ -1,7 +1,7 @@
 var settingsFileName = "settings.xml"
 var settingsFolder = File($.fileName).parent.fsName + "/settings/";
 var macXMLFolder = "/Users/David/Dropbox/Development/Adobe/Excel/";
-var pcXMLFolder = "/C/Users/David/Documents/vscode/Adobe_Versioning/xml/";
+var pcXMLFolder = "/E/Development/xml/";
 
 var defaultSettings = {
   macXMLPath: macXMLFolder,
