236d86f217e18812734ea6587a2ad03fc17c05da
David Fuller <david@muvi2.co.uk>
2020-10-02 11:53:45 +0100

    Added render profile to saveFrame and undid hard wiring of frame rate

--- Render_Stuff.jsx
+++ Render_Stuff.jsx
@@ -1,13 +1,14 @@
-function saveFrame(theComp, frameNumber){
+function saveFrame(theComp, frameNumber, renderTemplate){
   if (theComp instanceof CompItem){
     var renderItem = app.project.renderQueue.items.add(theComp);
     var outputModule = renderItem.outputModule(1);
     var outputFolder = "/Users/David/Dropbox/Development/Adobe/Stills/";
 
-    outputModule.applyTemplate("PNG");
+    outputModule.applyTemplate(renderTemplate);
+    var omSettings = outputModule.getSetting("Output File Info")
     outputModule.file = File(outputFolder + theComp.name + "_[#]");
-    renderItem.timeSpanStart = frameNumber/25;
-    renderItem.timeSpanDuration = 1/25; 
+    renderItem.timeSpanStart = frameNumber/theComp.frameRate;
+    renderItem.timeSpanDuration = 1/theComp.frameRate; 
     app.project.renderQueue.render()
 
     var renderedFile = new File(outputFolder + theComp.name + "_" + frameNumber +".png");
