f7aa07c34e7d75952e4c6d93d8d576800af848d5
David Fuller <david@muvi2.co.uk>
2020-10-15 16:04:14 +0100

    Change SaveFrame to be extension independent

--- Render_Stuff.jsx
+++ Render_Stuff.jsx
@@ -5,14 +5,25 @@ function saveFrame(theComp, frameNumber, renderTemplate){
     var outputFolder = "/Users/David/Dropbox/Development/Adobe/Stills/";
 
     outputModule.applyTemplate(renderTemplate);
-    var omSettings = outputModule.getSetting("Output File Info")
+    var omSettings = outputModule.getSettings(GetSettingsFormat.STRING)
+    var omFileInfo = omSettings["Output File Info"]
+    
     outputModule.file = File(outputFolder + theComp.name + "_[#]");
     renderItem.timeSpanStart = frameNumber/theComp.frameRate;
     renderItem.timeSpanDuration = 1/theComp.frameRate; 
     app.project.renderQueue.render()
 
-    var renderedFile = new File(outputFolder + theComp.name + "_" + frameNumber +".png");
-    $.writeln(renderedFile.rename(theComp.name + ".png"))
+    var ext = getExtension(omFileInfo["Full Flat Path"]);
+    if (ext != null){
+      var renderedFile = new File(outputFolder + theComp.name + "_" + frameNumber + ext);
+      if (renderedFile.exists){
+        var renamedFile = new File(outputFolder + theComp.name + ext);
+        if (renamedFile.exists){
+          renamedFile.remove()
+        }
+      }
+      $.writeln(renderedFile.rename(theComp.name + ext))
+    }
   }
 }
 function renderMovie(theComp){
@@ -25,4 +36,15 @@ function renderMovie(theComp){
     outputModule.file = File(outputFolder + theComp.name);
     app.project.renderQueue.render();
   } 
+}
+
+function getExtension(filename){
+
+  var finalDotPosition = filename.lastIndexOf(".");
+  if (finalDotPosition > -1){
+    return filename.substring(finalDotPosition)
+  } else {
+    return null
+  }
+
 }
