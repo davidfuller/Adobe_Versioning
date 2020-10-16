847d9bb64b382cb45218f5c0a2c15e6dd23efc37
David Fuller <david@muvi2.co.uk>
2020-10-16 18:13:41 +0100

    Added comp profile name and textLayers and renders

--- globals.jsx
+++ globals.jsx
@@ -4,6 +4,10 @@ var settingsFolder = File($.fileName).parent.fsName + "/settings/";
 var macXMLFolder = "/Users/David/Dropbox/Development/Adobe/Excel/";
 var pcXMLFolder = "/C/Users/David/Documents/vscode/Adobe_Versioning/xml/";
 
+var compProfileFileName = "comp_profile.json"
+var textLayerNames = ["Title", "Message", "Navigation"]
+var renderNames = ["Still", "Clip"]
+
 var defaultSettings = {
   macXMLPath: macXMLFolder,
   pcXMLPath: pcXMLFolder
