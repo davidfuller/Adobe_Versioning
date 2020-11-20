14fab12fda69f656188709774be113a4c1972a70
David Fuller <david@muvi2.co.uk>
2020-11-20 14:56:09 +0000

    Added addDropDown

--- Generic.jsx
+++ Generic.jsx
@@ -11,3 +11,32 @@ function fileNameWithoutExtension(theFile){
   }
   return theName;
 }
+
+/**
+ * 
+ * @param {Group} parentGrp 
+ * @param {string} labelText 
+ * @param {string} dropdownDefaultText 
+ * @param {StaticText} nameStaticText
+ * @returns {DropDownList}
+ */
+
+function addDropDown(parentGrp, labelText, dropdownDefaultText, nameStaticText){
+  var grp = parentGrp.add("group");
+  grp.orientation = "row";
+  grp.alignment = "left"
+  
+  var label = grp.add("statictext", undefined, labelText)
+  label.size = [60,12];
+
+  var dropdown = grp.add("dropdownlist", undefined, []);
+  dropdown.add("item", dropdownDefaultText);
+  dropdown.selection = 0;
+  dropdown.size = [280, 22];
+
+  nameStaticText = grp.add("statictext",undefined,"")
+  nameStaticText.size = [280,12]
+
+
+  return dropdown
+}
