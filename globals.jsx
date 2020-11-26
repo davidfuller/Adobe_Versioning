dc7165665a44d3b4b1fd57fd375b269c149165e8
David Fuller <david@muvi2.co.uk>
2020-11-26 11:23:22 +0000

    Created textMapping object

--- globals.jsx
+++ globals.jsx
@@ -7,8 +7,41 @@ var macOutputFolder = "/Users/David/Dropbox/Development/Adobe/Movies/";
 var pcOutputFolder = "/C/Users/David/Documents/vscode/Adobe_Versioning/movies/";
 
 var compProfileFileName = "comp_profile.json"
-var textLayerNames = ["1L Title", "1L Message", "1L Navigation", "2L Title", "2L Message", "2L Navigation"]
+//var textLayerNames = ["1L Title", "1L Message", "1L Navigation", "2L Title", "2L Message", "2L Navigation"]
 var renderNames = ["Still", "Clip"]
+
+var textMapping = [
+  {
+    name: "1L Title",
+    profileField: "1L Title",
+    dataField: "oneLineTitle"
+  },
+  {
+    name: "1L Message",
+    profileField: "1L Message",
+    dataField: "oneLineMessage"
+  },
+  {
+    name: "1L Navigation",
+    profileField: "1L Navigation",
+    dataField: "oneLineNavigation"
+  },
+  {
+    name: "2L Title",
+    profileField: "2L Title",
+    dataField: "twoLineTitle"
+  },
+  {
+    name: "2L Message",
+    profileField: "2L Message",
+    dataField: "twoLineMessage"
+  },
+  {
+    name: "2L Navigation",
+    profileField: "2L Navigation",
+    dataField: "twoLineNavigation"
+  }
+]
 
 var defaultSettings = {
   macXMLPath: macXMLFolder,
